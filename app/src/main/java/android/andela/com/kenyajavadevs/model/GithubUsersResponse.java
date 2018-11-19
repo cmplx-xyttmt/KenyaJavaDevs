408f1d83c83dbeacd45a62b366e91d3c09e8fdbb
cmplx-xyttmt <archimedesisaac@gmail.com>
2018-11-19 17:36:20 +0300

    ft(api-endpoints): define api endpoints
    - setup api endpoint for getting users according to a specific query
    - setup api endpoint for getting a single user
    
    [Finishes #155903085]

--- app/src/main/java/android/andela/com/kenyajavadevs/model/GithubUsersResponse.java
+++ app/src/main/java/android/andela/com/kenyajavadevs/model/GithubUsersResponse.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class GithubUsersResponse {
 
-    @SerializedName("")
+    @SerializedName("items")
     private List<GithubUser> githubUsers;
 
     public GithubUsersResponse() {
