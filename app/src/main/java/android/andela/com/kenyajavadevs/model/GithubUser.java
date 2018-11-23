470bbd45824af7ad115c26d08242b4e0bde092ac
cmplx-xyttmt <archimedesisaac@gmail.com>
2018-11-23 11:59:28 +0300

    ft(handle-config-changes): Handle configuration changes
    - make the GithubUser class implement Parcelable
    - ensure page doesn't refresh when screen is rotated
    
    [Finishes #156005187]

--- app/src/main/java/android/andela/com/kenyajavadevs/model/GithubUser.java
+++ app/src/main/java/android/andela/com/kenyajavadevs/model/GithubUser.java
@@ -1,8 +1,11 @@
 package android.andela.com.kenyajavadevs.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
-public class GithubUser {
+public class GithubUser implements Parcelable {
 
     @SerializedName("login")
     private String username;
@@ -30,5 +33,38 @@ public class GithubUser {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(username);
+        dest.writeString(avatarUrl);
+        dest.writeString(url);
+        dest.writeString(name);
+    }
+
+    public static final Parcelable.Creator<GithubUser> CREATOR
+            = new Parcelable.Creator<GithubUser>() {
+        @Override
+        public GithubUser createFromParcel(Parcel source) {
+            return new GithubUser(source);
+        }
+
+        @Override
+        public GithubUser[] newArray(int size) {
+            return new GithubUser[size];
+        }
+    };
+
+    private GithubUser(Parcel in) {
+        username = in.readString();
+        avatarUrl = in.readString();
+        url = in.readString();
+        name = in.readString();
     }
 }
--- app/src/main/java/android/andela/com/kenyajavadevs/view/MainActivity.java
+++ app/src/main/java/android/andela/com/kenyajavadevs/view/MainActivity.java
@@ -4,6 +4,7 @@ import android.andela.com.kenyajavadevs.R;
 import android.andela.com.kenyajavadevs.adapter.GithubUsersAdapter;
 import android.andela.com.kenyajavadevs.model.GithubUser;
 import android.andela.com.kenyajavadevs.presenter.GithubPresenter;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,10 +14,12 @@ import android.widget.Toast;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements ProfileListView {
+    public static final String LIST_STATE_KEY = "users_list_state";
     public static final String EXTRA_USERNAME = "android.andela.com.kenyajavadevs.extra.USERNAME";
     public static final String EXTRA_PROFILE_LINK = "android.andela.com.kenyajavadevs.extra.PROFILE_LINK";
     // TODO: Also put an extra for the profile image.
 
+    Parcelable githubUsersListState;
     LinearLayoutManager mLayoutManager;
 
     @Override
@@ -43,5 +46,27 @@ public class MainActivity extends AppCompatActivity implements ProfileListView {
         Toast.makeText(this,
                 "Something went wrong: " + throwable.getMessage(),
                 Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        githubUsersListState = mLayoutManager.onSaveInstanceState();
+        outState.putParcelable(LIST_STATE_KEY, githubUsersListState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null)
+            githubUsersListState = savedInstanceState.getParcelable(LIST_STATE_KEY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (githubUsersListState != null) {
+            mLayoutManager.onRestoreInstanceState(githubUsersListState);
+        }
     }
 }
